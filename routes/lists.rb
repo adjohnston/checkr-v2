e2c4f8906be7bc789b1871fa1d015036e92776b3
adjohnston <adam.johnston3679@gmail.com>
2014-10-15 15:36:43 +0100

    added routes for lists

--- routes/lists.rb
+++ routes/lists.rb
@@ -8,6 +8,47 @@ module Sinatra
           headers 'access-control-allow-origin'  => '*',
                   'access-control-allow-methods' => ['options', 'get', 'post']
         end
+
+        app.get '/user/:username/lists' do
+          user  = User.first(username: params[:username])
+          lists = List.all(user: user).to_json
+        end
+
+        app.get '/user/:username/list/:id' do
+          user = User.first(username: params[:username])
+          list = List.first(id: params[:id], user: user).to_json
+        end
+
+        app.post '/user/:username/list/create' do
+          user = User.first(username: params[:username])
+          list = List.new({
+            user:      user,
+            name:      params[:name],
+            desc:      params[:desc],
+            is_public: params[:is_public]
+          })
+
+          list.save
+        end
+
+        app.put '/user/:username/list/:id' do
+          user = User.first(username: params[:username])
+          list = List.first(id: params[:id], user: user)
+
+          list.update({
+            name:      params[:name],
+            desc:      params[:desc],
+            is_public: params[:is_public]
+          })
+
+          list.save
+        end
+
+        app.delete '/user/:username/list/:id' do
+          user = User.first(username: params[:username])
+          list = List.first(id: params[:id], user: user)
+          list.delete
+        end
       end
 
     end
